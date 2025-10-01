6f32da8cc55d08f49955b4b73f00d9bffe4fc441
M307user <M307user@lab.uc-bcf.local>
2025-10-01 11:03:20 +0800

    First Commit

---
+++ CIT17/PHP-Exercise/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lab Activity 02</h1>
+    <ul>
+        <li><a href="intro.php"></a>Introduce Yourself</li>
+        <li><a href="simple_math.php"></a>Simple Math</li>
+        <li><a href="perimeter.php"></a>Area and Perimeter of a Rectangle</li>
+        <li><a href="temperature.php"></a>Temperature Calculator</li>
+        <li><a href="swap_var.php"></a>Swapping Variables</li>
+        <li><a href="salary_calc.php"></a>Salary Calculator</li>
+        <li><a href="bmi_calc.php"></a>BMI Calculator</li>
+        <li><a href="str_manipulation.php"></a>String Manipulation</li>
+        <li><a href="bank_acc_simulation.php"></a>Bank Account Simulation</li>
+        <li><a href="grading_system.php"></a>Simple Grading System</li>
+        <li><a href="currency_calc.php"></a>Currency Converter</li>
+        <li><a href="tcost_estimator.php"></a>Travel Cost Estimator</li>
+    </ul>
+</body>
+</html>
---
+++ CIT17/PHP-Exercise/intro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $name = "Adrian G. Adora";
+        $age = 20;
+        $fave_color ="Navy Blue";
+
+        echo "<h1>Hello! I am $name. $age years of age. My favorite color is $fave_color</h1>";
+    ?>
+</body>
+</html>
