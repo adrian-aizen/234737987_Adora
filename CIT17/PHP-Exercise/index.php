8c4604295d6008a46afd3e8ac9c33bf511e70b5f
M307user <M307user@lab.uc-bcf.local>
2025-10-01 11:20:44 +0800

    Initial Commit

--- CIT17/PHP-Exercise/index.php
+++ CIT17/PHP-Exercise/index.php
@@ -8,18 +8,18 @@
 <body>
     <h1>Lab Activity 02</h1>
     <ul>
-        <li><a href="intro.php"></a>Introduce Yourself</li>
-        <li><a href="simple_math.php"></a>Simple Math</li>
-        <li><a href="perimeter.php"></a>Area and Perimeter of a Rectangle</li>
-        <li><a href="temperature.php"></a>Temperature Calculator</li>
-        <li><a href="swap_var.php"></a>Swapping Variables</li>
-        <li><a href="salary_calc.php"></a>Salary Calculator</li>
-        <li><a href="bmi_calc.php"></a>BMI Calculator</li>
-        <li><a href="str_manipulation.php"></a>String Manipulation</li>
-        <li><a href="bank_acc_simulation.php"></a>Bank Account Simulation</li>
-        <li><a href="grading_system.php"></a>Simple Grading System</li>
-        <li><a href="currency_calc.php"></a>Currency Converter</li>
-        <li><a href="tcost_estimator.php"></a>Travel Cost Estimator</li>
+        <li><a href="intro.php">Introduce Yourself</a></li>
+        <li><a href="math.php">Simple Math</a></li>
+        <li><a href="perimeter.php">Area and Perimeter of a Rectangle</a></li>
+        <li><a href="temperature.php">Temperature Calculator</a></li>
+        <li><a href="swap_var.php">Swapping Variables</a></li>
+        <li><a href="salary_calc.php">Salary Calculator</a></li>
+        <li><a href="bmi_calc.php">BMI Calculator</a></li>
+        <li><a href="str_manipulation.php">String Manipulation</a></li>
+        <li><a href="bank_acc_simulation.php">Bank Account Simulation</a></li>
+        <li><a href="grading_system.php">Simple Grading System</a></li>
+        <li><a href="currency_calc.php">Currency Converter</a></li>
+        <li><a href="tcost_estimator.php">Travel Cost Estimator</a></li>
     </ul>
 </body>
 </html>
---
+++ CIT17/PHP-Exercise/math.php
@@ -0,0 +1,15 @@
+<?php 
+    $a = 100;
+    $b = 5;
+
+    $sum = $a + $b;
+    $difference = $a - $b;
+    $product = $a * $b;
+    $quotient = $a / $b;
+
+echo "<p>Sum: </p>";
+echo "<p>Difference: </p>";
+echo "<p>Product: </p>";
+echo "<p>Quotient: </p>";
+?>
+
